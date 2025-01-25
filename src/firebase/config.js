1de3666aad6b55238e62968e1abddbd9162795b7
Hafiz Usama <160656348+usamahafiz@users.noreply.github.com>
2025-01-25 14:55:23 +0500

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,13 +5,13 @@ import { getStorage, ref, uploadBytes, uploadBytesResumable } from "firebase/sto
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:  "AIzaSyCNr7SKDdbBPHoWNFihy-h_Hl39MNne5Oo",
-  authDomain: "e-commerce-app-001.firebaseapp.com",
-  projectId: "e-commerce-app-001",
-  storageBucket: "e-commerce-app-001.appspot.com",
-  messagingSenderId:  "539834128246",
-  appId: "1:539834128246:web:0c2138865483f128a0cc57",
-  measurementId: "G-GHP0KQ5RE1"
+  apiKey:  "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId:  "",
+  appId: "",
+  measurementId: ""
 };
 
 
